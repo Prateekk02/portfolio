d3e4371be823c463a4d9e64382e0a9395a87a724
Prateekk02 <prateek.kumar2002@gmail.com>
2025-05-24 23:33:32 +0530

    Updated Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -164,9 +164,9 @@ const Hero = () => {
         </motion.h1>
         <motion.div
           initial={{ opacity: 0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3, ease: "easeOut", delay: 0.1 }}
-          viewport={{ once: true }}
+          viewport={{ once: true, amount:0.2 }}
         >
           <LongCard itemList={blogItems} />
         </motion.div>
