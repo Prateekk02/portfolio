1c273bec673ffc886e72b4cd737e8050f77c7256
Prateekk02 <prateek.kumar2002@gmail.com>
2025-06-13 16:05:50 +0530

    Fixed deployment issue

--- components/ProjectHero.tsx
+++ components/ProjectHero.tsx
@@ -3,13 +3,14 @@ import Header from './Header'
 import ProjectLister from './ProjectLister'
 import { projectItems } from '@/data/content'
 
+
 const ProjectHero = () => {
     const visibleProjectCount = projectItems.length;
   return (
     <div>
         <Header title='Projects' content="I've developed various projects across various domains that includes Python, Machine Learning, Deep Learning, NLP, Computer Vision, Gen AI, Full stack projects using Next.js and AI SaaS. " />
         <div className="mt-10">
-            <ProjectLister count={visibleProjectCount}/>
+            <ProjectLister count={visibleProjectCount} projectItem={projectItems}/>
         </div>
     </div>
   )
